204ca768975bed6d75df533091b5c6a467044555
Viki Flendiansyah <vfsyah@gmail.com>
2024-06-08 22:51:14 +0700

    feat: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const isLogin = false;
+  // if (request.nextUrl.pathname.startsWith("/product"))
+  //   if (!isLogin) {
+  //     return NextResponse.redirect(new URL("/login", request.url));
+  //   }
+  if (!isLogin) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/about/:path*"],
+};
